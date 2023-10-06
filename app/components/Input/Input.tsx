1de361f7c2ba18ae205b22870e317910c9a89644
sllanos <sergio.llanos.garcia@gmail.com>
2023-10-06 09:29:02 -0500

    Add a icon detail to show and hide the pass

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,17 +1,39 @@
 "use client";
+import Image from "next/image";
 import { ChangeEvent, useState } from "react";
+import hidePassIcon from "../../assets/hide-pass.png";
+import showPassIcon from "../../assets/show-pass.png";
 
 const Input = ({
   onChange,
 }: {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }) => {
+  const [type, setType] = useState("password");
+
+  const toogleType = () => {
+    if (type === "password") {
+      setType("text");
+      return;
+    }
+    setType("password");
+  };
   return (
-    <input
-      className="h-7 border border-current focus:outline-blue-500 focus:shadow-md"
-      type="text"
-      onChange={onChange}
-    />
+    <div className="flex border mx-5">
+      <input
+        className="h-7 border-current focus:outline-blue-500 focus:shadow-md"
+        type={type}
+        onChange={onChange}
+      />
+      <button className="px-2" onClick={toogleType}>
+        <Image
+          src={type === "password" ? showPassIcon : hidePassIcon}
+          width={20}
+          height={20}
+          alt="pass-icon"
+        />
+      </button>
+    </div>
   );
 };
 
